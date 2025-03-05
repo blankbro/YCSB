2c8b732cc939170a6a75769b8c344b9789587a1f
lizexin <zexin-li@outlook.com>
2025-03-05 15:59:02 +0800

    select 加 /*+ full_series */

--- influxdb1.8/src/main/java/site/ycsb/db/influxdb1_8/InfluxdbHelper.java
+++ influxdb1.8/src/main/java/site/ycsb/db/influxdb1_8/InfluxdbHelper.java
@@ -119,7 +119,8 @@ public class InfluxdbHelper {
                                         long startTime, long endTime) throws Exception {
 
     String fieldStr = fields == null || fields.isEmpty() ? "*" : String.join(", ", fields);
-    String sql = String.format("select /*+ full_series */ %s from \"%s\".\"%s\".\"%s\" where time >= %sms and time <= %sms",
+    String sql = String.format(
+            "select /*+ full_series */ %s from \"%s\".\"%s\".\"%s\" where time >= %sms and time <= %sms",
             fieldStr, database, rpName, measurement, startTime, endTime);
 
     String whereSql = where.entrySet().stream()
